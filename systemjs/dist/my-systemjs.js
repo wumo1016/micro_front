a5ca51c28a7a2785c641dff61daf53ebd11cdd4d
wumo1016 <760478279@qq.com>
2022-04-01 20:05:09 +0800

    chore

--- systemjs/dist/my-systemjs.js
+++ systemjs/dist/my-systemjs.js
@@ -1,11 +1,22 @@
+let currentRegister
+
 function SystemJS() {}
 
 function loadScript(url) {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script')
     script.src = url
-    // script.async = true
-    script.onload = resolve
+    script.async = true
+    script.onload = () => {
+      const _currentRegister = currentRegister
+      // 没有其他依赖
+      if (!_currentRegister) {
+        resolve([[], function () {}])
+      }
+      resolve(_currentRegister)
+      currentRegister = undefined
+      document.head.removeChild(script)
+    }
     script.onerror = reject
     document.head.appendChild(script)
   })
@@ -21,31 +32,59 @@ SystemJS.prototype.import = function (path) {
       resolve(baseUrl + path.slice(1))
     }
   }).then(url => {
+    let exec
     return loadScript(url)
+      .then(register => {
+        function _export() {}
+        const { setters, execute } = register[1](_export) // {setters: Array(2), execute: ƒ}
+        exec = execute
+        return [register[0], setters]
+      })
+      .then(([deps, setters]) => {
+        const imports = loadImports() // { react: url, react-dom: url }
+        return Promise.all(
+          deps.map((dep, index) => {
+            const setter = setters[index]
+            const url = imports[dep]
+            return loadScript(url).then(res => {
+              const p = getLastGlobal()
+              p.default = p // 取属性的时候默认都加了default
+              if (p) setter(p)
+            })
+          })
+        )
+      })
+      .then(() => exec())
   })
 }
 
-SystemJS.prototype.register = async function (modules, callback) {
-  const fn = function (data) {
-    console.log(123);
-    console.log(data);
-  }
-  const { setters, execute } = callback(fn)
+// 将本次声明的以来和声明 暴露到到外部
+SystemJS.prototype.register = async function (deps, callback) {
+  currentRegister = [deps, callback]
+}
+
+function loadImports() {
   const scriptList = document.querySelectorAll('script')
   const target = [...scriptList].find(dom => dom.type === 'systemjs-importmap')
-  if (!target) return
-  try {
-    const { imports } = JSON.parse(target.innerHTML.trim())
-    await Promise.all(
-      modules.map(async key => {
-        await loadScript(imports[key])
-        return 1
-      })
-    )
-    setters[0](window.React)
-    setters[1](window.ReactDOM)
-    const data = execute()
-  } catch {}
+  const { imports } = JSON.parse(target.innerHTML.trim())
+  return imports || {}
 }
+
+// 获取此刻window上挂载的全局变量
+let set = new Set()
+function saveGlobalData() {
+  for (const key in window) {
+    set.add(key)
+  }
+}
+function getLastGlobal() {
+  for (const key in window) {
+    if (!set.has(key)) {
+      set.add(key)
+      return window[key]
+    }
+  }
+}
+saveGlobalData()
 
 var System = new SystemJS()
